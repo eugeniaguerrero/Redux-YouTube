07850c0f7e008c3c918271b1e26e0362f54825b0
Eugenia Guerrero <angeleuge95@gmail.com>
2017-06-14 17:27:56 +0100

    adds search bar component

---
+++ src/components/search_bar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SearchBar = () => {
+  // this will generate HTML that the user can type input into
+  return <input />;
+};
+
+//any file that imports SearchBar will get this component
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// Create a new component. This component should produce
-// some HTML.
 
-// const App = function() { <= old ES syntax
+// path reference as this is a file we have created
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyAT3JscCLJsscSW7McUF8KSBeSFgplL-Sg';
+
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+  <div>
+    <SearchBar />
+  </div>
+  );
 }
 
-// Take this component's generated HTML and put it on
-// the page (in the DOM).
 ReactDOM.render(<App />, document.querySelector('.container'));
