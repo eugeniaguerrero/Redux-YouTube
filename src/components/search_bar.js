f58b48b2ed419f3f674339bb8585aacf2c6b5df4
Eugenia Guerrero <angeleuge95@gmail.com>
2017-06-16 14:27:57 +0100

    adds controlled components

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,8 +2,19 @@ import React, { Component } from 'react';
 
 //define a new class and give it access to all of the functionality that React.Component does
 class SearchBar extends Component {
+  constructor(props) {
+    super(props); //calls the parent method on the parent class
+    this.state = {term: ''}; //
+  }
+
   render() {
-    return <input onChange={event => console.log(event.target.value)} />;
+    return (
+      <div>
+        <input
+          value = {this.state.term}
+          onChange={event => this.setState({ term: event.target.value})} />
+      </div>
+    );
   }
 }
 
